ca870ac2f3b45dca915cbc26c4fe59151be94525
Medrine-Jepkemoi <99050771+Medrine-Jepkemoi@users.noreply.github.com>
2023-12-07 01:54:23 +0300

    Landing page background image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import HomeImage from "./images/homeimg.png";
+import NavBar from "../components/NavBar";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div style={{ backgroundImage: `url(${HomeImage})` }} className="h-screen w-screen bg-no-repeat bg-cover overflow-hidden " >
+      <NavBar />
+      <div >Home</div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
