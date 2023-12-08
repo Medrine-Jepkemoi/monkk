e907c9fef047d8cbf0c8f83d44efd056d707b6c8
Medrine-Jepkemoi <99050771+Medrine-Jepkemoi@users.noreply.github.com>
2023-12-08 13:32:46 +0300

    Basic home page structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import HomeImage from "./images/homeimg.png";
 import NavBar from "../components/NavBar";
+import BuyYIcon from "./images/buyYIcon.svg"
 
 const Home = () => {
   return (
-    <div style={{ backgroundImage: `url(${HomeImage})` }} className="h-screen w-screen bg-no-repeat bg-cover overflow-hidden " >
-      <NavBar />
-      <div >Home.</div>
+    <div style={{ backgroundImage: `url(${HomeImage})`, height: "100vh", width: "100vw"}} className=" h-full w-full bg-no-repeat bg-cover overflow-scroll" >
+      <NavBar/>
+      <div className="pl-8  w-full h-full flex flex-col items-start justify-center ">
+        <p className="text-white text-lg">At Monk we believe in <span className="text-yellow-400">Kindness</span> & <span className="text-yellow-400">Being Humble</span></p>
+        <button className="flex items-center justify-center gap-2 h-14 w-fit bg-black rounded-md border-solid px-4 mt-6">
+          <img src={BuyYIcon} alt="Buy Icon" className="h-8 w-8" />
+          <p className="text-yellow-400 text-base">Get Exclusive Merch</p>
+        </button>
+      </div>
     </div>
   );
 };
